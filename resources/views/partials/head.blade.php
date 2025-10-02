2eeb0750964301a65b2dc2a42b05e13b6031505a
Emmanuel Wediah <100675006+Wediah@users.noreply.github.com>
2025-10-02 23:15:41 +0000

    no image

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -10,16 +10,9 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-@production
-    <!-- Production: Use compiled assets -->
-    @php
-        $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
-    @endphp
-    @if(isset($manifest['resources/css/app.css']))
-        <link rel="stylesheet" href="{{ asset('build/' . $manifest['resources/css/app.css']['file']) }}">
-    @endif
+@if(app()->isProduction())
+    <link rel="stylesheet" href="{{ asset('build/assets/app-BNumen2h.css') }}">
 @else
-        <!-- Development: Use Vite -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-@endproduction
+@endif
 @fluxAppearance
