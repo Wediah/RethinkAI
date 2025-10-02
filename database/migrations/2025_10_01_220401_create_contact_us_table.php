23f28270c930118e4c1026d6cce0aebbfb6cdf9c
Emmanuel Wediah <100675006+Wediah@users.noreply.github.com>
2025-10-02 22:35:09 +0000

    no image

--- database/migrations/2025_10_01_220401_create_contact_us_table.php
+++ database/migrations/2025_10_01_220401_create_contact_us_table.php
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('contact_u_s');
+        Schema::dropIfExists('contact_us');
     }
 };
