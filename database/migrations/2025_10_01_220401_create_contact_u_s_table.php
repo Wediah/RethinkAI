dee5bc501944a5de79e35284aaa4d16defeb87e0
Emmanuel Wediah <100675006+Wediah@users.noreply.github.com>
2025-10-02 22:16:29 +0000

    no image

--- database/migrations/2025_10_01_220401_create_contact_u_s_table.php
+++ database/migrations/2025_10_01_220401_create_contact_u_s_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('contact_u_s', function (Blueprint $table) {
+        Schema::create('contact_us', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email');
