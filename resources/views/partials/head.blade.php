330b9c714b9a1987120ff21ed33ba9498e67c646
Emmanuel Wediah <100675006+Wediah@users.noreply.github.com>
2025-10-03 00:01:33 +0000

    no image

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,15 +3,13 @@
 
 <title>{{ $title ?? config('app.name') }}</title>
 
-<link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="icon" href="{{ asset('assets/log.png') }}" type="image/x-icon">
+@vite('resources/css/app.css')
+
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-
 <script src="https://cdn.tailwindcss.com"></script>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 @fluxAppearance
